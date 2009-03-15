puzzlet/html5lib

commit d4fe4e13a5bf323bc7614209bb85b4625811ee24

Resync my tree with the trunk. Adds support for coercing trees to xml infosets in particular for lxml (still need to wire up the tests) and some speed improvements in the parser. Big apologies for the large checkin, there are some regressions in the liberal xml parser and the sanitizer that need to be fixed.
git-svn-id: http://html5lib.googlecode.com/svn/trunk@1248 acbfec75-9323-0410-a652-858a13e371e0

python/src/html5lib/sanitizer.py
@@ -1,6 +1,8 @@
 import re
 from xml.sax.saxutils import escape, unescape
+
 from tokenizer import HTMLTokenizer
+from constants import tokenTypes
 
 class HTMLSanitizerMixin(object):
     """ sanitization of XHTML+MathML+SVG and of inline style attributes."""
@@ -138,41 +140,49 @@ class HTMLSanitizerMixin(object):
     #   sanitize_html('<a href="javascript: sucker();">Click here for $100</a>')
     #    => <a>Click here for $100</a>
     def sanitize_token(self, token):
-        if token["type"] in ["StartTag", "EndTag", "EmptyTag"]:
+        if token["type"] in (tokenTypes["StartTag"], tokenTypes["EndTag"], 
+                             tokenTypes["EmptyTag"]):
             if token["name"] in self.allowed_elements:
                 if token.has_key("data"):
-                    attrs = dict([(name,val) for name,val in token["data"][::-1] if name in self.allowed_attributes])
+                    attrs = dict([(name,val) for name,val in
+                                  token["data"][::-1] 
+                                  if name in self.allowed_attributes])
                     for attr in self.attr_val_is_uri:
                         if not attrs.has_key(attr):
                             continue
-                        val_unescaped = re.sub("[`\000-\040\177-\240\s]+", '', unescape(attrs[attr])).lower()
-                        if re.match("^[a-z0-9][-+.a-z0-9]*:",val_unescaped) and (val_unescaped.split(':')[0] not in self.allowed_protocols):
+                        val_unescaped = re.sub("[`\000-\040\177-\240\s]+", '',
+                                               unescape(attrs[attr])).lower()
+                        if (re.match("^[a-z0-9][-+.a-z0-9]*:",val_unescaped) or
+                            (val_unescaped.split(':')[0] not in 
+                             self.allowed_protocols)):
                             del attrs[attr]
                     for attr in self.svg_attr_val_allows_ref:
                         if attr in attrs:
-                            attrs[attr] = re.sub(r'url\s*\(\s*[^#\s][^)]+?\)', ' ',
+                            attrs[attr] = re.sub(r'url\s*\(\s*[^#\s][^)]+?\)',
+                                                 ' ',
                                                  unescape(attrs[attr]))
                     if (token["name"] in self.svg_allow_local_href and
-                        'xlink:href' in attrs and re.find('^\s*[^#\s].*', attrs['xlink:href'])):
+                        'xlink:href' in attrs and re.find('^\s*[^#\s].*',
+                                                          attrs['xlink:href'])):
                         del attrs['xlink:href']
                     if attrs.has_key('style'):
                         attrs['style'] = self.sanitize_css(attrs['style'])
                     token["data"] = [[name,val] for name,val in attrs.items()]
                 return token
             else:
-                if token["type"] == "EndTag":
+                if token["type"] == tokenTypes["EndTag"]:
                     token["data"] = "</%s>" % token["name"]
                 elif token["data"]:
                     attrs = ''.join([' %s="%s"' % (k,escape(v)) for k,v in token["data"]])
                     token["data"] = "<%s%s>" % (token["name"],attrs)
                 else:
                     token["data"] = "<%s>" % token["name"]
-                if token["type"] == "EmptyTag":
+                if token["type"] == tokenTypes["EmptyTag"]:
                     token["data"]=token["data"][:-1] + "/>"
-                token["type"] = "Characters"
+                token["type"] = tokenTypes["Characters"]
                 del token["name"]
                 return token
-        elif token["type"] == "Comment":
+        elif token["type"] == tokenTypes["Comment"]:
             pass
         else:
             return token
@@ -190,7 +200,8 @@ class HTMLSanitizerMixin(object):
           if not value: continue
           if prop.lower() in self.allowed_css_properties:
               clean.append(prop + ': ' + value + ';')
-          elif prop.split('-')[0].lower() in ['background','border','margin','padding']:
+          elif prop.split('-')[0].lower() in ['background','border','margin',
+                                              'padding']:
               for keyword in value.split():
                   if not keyword in self.acceptable_css_keywords and \
                       not re.match("^(#[0-9a-f]+|rgb\(\d+%?,\d*%?,?\d*%?\)?|\d{0,2}\.?\d{0,2}(cm|em|ex|in|mm|pc|pt|px|%|,|\))?)$",keyword):

c/chtml5lib/charset.c
@@ -1,15 +1,17 @@
 #include "charset.h"
 
-struct vstr *get_encoding(FILE *stream) {
+struct vstr *get_encoding(FILE *stream, num_bytes_meta) {
     
     //Read the first 512 bytes of the stream into an array
-    char buf[BUF_SIZE+1];
+    char buf[num_bytes_meta+1];
     int n;
     struct vstr *encoding;
     
-    encoding = vstr_new();
+    encoding = vstr_new(8);
     
     n = fread(buf, 1, BUF_SIZE, stream);
+    //Reset the buffer position
+    fseek(stream, -n, SEEK_CUR);
     buf[n] = '\0';
     
     //Check for byte order marks
@@ -136,8 +138,8 @@ char *get_attr(char *buf, struct attr *attr_value) {
     
     int spaces = 0; //Do the spaces step
     
-    attr_value->name = vstr_new();
-    attr_value->value = vstr_new();
+    attr_value->name = vstr_new(8);
+    attr_value->value = vstr_new(8);
     
     *(attr_value->name->str) = '\0';
     *(attr_value->value->str) = '\0';
@@ -225,7 +227,7 @@ struct vstr *handle_content_type(struct vstr *attr_value) {
     char *value;
     char *quote;
     
-    encoding = vstr_new();
+    encoding = vstr_new(8);
     value = attr_value->str;
     //Skip characters up to and including the first ;
     value = jump_to(value, ";");

c/chtml5lib/var_string.c
@@ -1,25 +1,41 @@
 #include "var_string.h"
 
-struct vstr *vstr_new() {
+struct vstr *vstr_new(size_t size) {
     struct vstr *rv; 
     
     rv = (struct vstr *)malloc(sizeof(struct vstr));
     if (rv == NULL) {
         abort();
     }
-    rv->str = (char *)malloc(DEFAULT_LENGTH * sizeof(char));
+    
+    rv->str = (char *)malloc(size * sizeof(char));
     if (rv->str == NULL) {
         abort();
     }
-    rv->memsize = DEFAULT_LENGTH-1;
+    
+    rv->size = size-1;
+    rv->pos = 0;
     vstr_clear(rv);
     return rv;
 };
+
+struct vstr *vstr_from_char(char *str) {
+    /*Create a new vstr that contains the same characters as an existing char 
+     buffer. This copies the characters rather than creating a reference*/
+    
+    struct vstr *rv;
+    int size;
+    size = strlen(str) + 1;
+    rv = vstr_new(size);
+    strcpy(rv->str, str);
+    return rv;
+}
 
 void vstr_free(struct vstr *str) {
     free(str->str);
     free(str);
 };
+
 
 void vstr_append(struct vstr *str, char *append_str) {
     vstr_append_n(str, append_str, strlen(append_str));
@@ -29,8 +45,8 @@ void vstr_append_n(struct vstr *str, char *append_str, size_t n) {
     int new_size;
     //Check if we have enough space to allocate the string
     new_size = n + strlen(str->str) + 1;
-    if (new_size >= str->memsize) {
-        vstr_setsize(str, 2*str->memsize);
+    if (new_size >= str->size) {
+        vstr_setsize(str, 2*str->size);
     }
     strncat(str->str, append_str, n);
 }
@@ -40,18 +56,21 @@ void vstr_setsize(struct vstr *str, int size) {
     if (str->str == NULL) {
         abort();
     }
-    str->memsize = size;
+    str->size = size;
 };
 
 int vstr_cmp(struct vstr *str, char *cmp_str) {
     return strcmp(str->str, cmp_str);
 };
 
+size_t vstr_strlen(struct vstr *str) {
+    return strlen(str->str);
+}
+
 struct vstr *vstr_duplicate(struct vstr *str) {
     //Duplicate an existing vstr
     struct vstr *new_str;
-    new_str = vstr_new();
-    vstr_setsize(new_str, str->memsize);
+    new_str = vstr_new(str->size);
     strcpy(new_str->str, str->str);
     return new_str;
 };

c/chtml5lib/var_string.h
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 struct vstr {
     char *str;
-    size_t memsize;
+    size_t size;
+	size_t pos;
 };
 
 #define DEFAULT_LENGTH 16

python/parse.py
@@ -12,7 +12,7 @@ from optparse import OptionParser
 #RELEASE remove
 sys.path.insert(0,os.path.abspath(os.path.join(__file__,'../src')))
 #END RELEASE
-from html5lib import html5parser, liberalxmlparser
+from html5lib import html5parser, liberalxmlparser, sanitizer, tokenizer
 from html5lib import treebuilders, serializer, treewalkers
 from html5lib import constants
 
@@ -46,10 +46,15 @@ def parse():
 
     treebuilder = treebuilders.getTreeBuilder(opts.treebuilder)
 
-    if opts.xml:
-        p = liberalxmlparser.XHTMLParser(tree=treebuilder)
+    if opts.sanitize:
+        tokenizer = sanitizer.HTMLSanitizer
     else:
-        p = html5parser.HTMLParser(tree=treebuilder)
+        tokenizer = HTMLTokenizer
+
+    if opts.xml:
+        p = liberalxmlparser.XHTMLParser(tree=treebuilder, tokenizer=tokenizer)
+    else:
+        p = html5parser.HTMLParser(tree=treebuilder, tokenizer=tokenizer)
 
     if opts.fragment:
         parseMethod = p.parseFragment
@@ -57,6 +62,7 @@ def parse():
         parseMethod = p.parse
 
     if opts.profile:
+        #XXX should import cProfile instead and use that
         import hotshot
         import hotshot.stats
         prof = hotshot.Profile('stats.prof')

python/src/html5lib/__init__.py
@@ -11,5 +11,7 @@ f = open("my_document.html")
 p = html5lib.HTMLParser()
 tree = p.parse(f) 
 """
-from html5parser import HTMLParser
+from html5parser import HTMLParser, parse
+from treebuilders import getTreeBuilder
+
 from liberalxmlparser import XMLParser, XHTMLParser

python/src/html5lib/constants.py
@@ -370,7 +370,6 @@ specialElements = frozenset((
 spaceCharacters = frozenset((
     u"\t",
     u"\n",
-    u"\u000B",
     u"\u000C",
     u" ",
     u"\r"
@@ -1087,6 +1086,17 @@ encodings = {
     'windows1258': 'cp1258',
     'windows936': 'gbk',
     'x-x-big5': 'big5'}
+
+tokenTypes = {
+    "Doctype":0,
+    "Characters":1,
+    "SpaceCharacters":2,
+    "StartTag":3,
+    "EndTag":4,
+    "EmptyTag":5,
+    "Comment":6,
+    "ParseError":7
+}
 
 class DataLossWarning(UserWarning):
     pass

python/src/html5lib/filters/validator.py
@@ -22,7 +22,7 @@ import _base
 import iso639codes
 import rfc3987
 import rfc2046
-from html5lib.constants import E, spaceCharacters, digits
+from html5lib.constants import E, spaceCharacters, digits, tokenTypes
 from html5lib import tokenizer
 import gettext
 _ = gettext.gettext
@@ -267,8 +267,9 @@ class HTMLConformanceChecker(_base.Filter):
         self.IDsWeHaveKnownAndLoved = []
 
     def __iter__(self):
+        types = dict((v,k) for k,v in tokenTypes.iteritems())
         for token in _base.Filter.__iter__(self):
-            fakeToken = {"type": token.get("type", "-"),
+            fakeToken = {"type": types.get(token.get("type", "-"), "-"),
                          "name": token.get("name", "-").capitalize()}
             method = getattr(self, "validate%(type)s%(name)s" % fakeToken, None)
             if method:
@@ -301,23 +302,23 @@ class HTMLConformanceChecker(_base.Filter):
         attrDict = dict([(name.lower(), value) for name, value in token.get("data", [])])
         inputType = attrDict.get("type", "text")
         if inputType not in inputTypeAllowedAttributeMap.keys():
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "unknown-input-type",
                    "datavars": {"attrValue": inputType}}
         allowedAttributes = inputTypeAllowedAttributeMap.get(inputType, [])
         for attrName, attrValue in attrDict.items():
             if attrName not in allowedAttributeMap['input']:
-                yield {"type": "ParseError",
+                yield {"type": tokenTypes["ParseError"],
                        "data": "unknown-attribute",
                        "datavars": {"tagName": "input",
                                     "attributeName": attrName}}
             elif attrName not in allowedAttributes:
-                yield {"type": "ParseError",
+                yield {"type": tokenTypes["ParseError"],
                        "data": "attribute-not-allowed-on-this-input-type",
                        "datavars": {"attributeName": attrName,
                                     "inputType": inputType}}
             if attrName in inputTypeDeprecatedAttributeMap.get(inputType, []):
-                yield {"type": "ParseError",
+                yield {"type": tokenTypes["ParseError"],
                        "data": "deprecated-attribute",
                        "datavars": {"attributeName": attrName,
                                     "inputType": inputType}}
@@ -330,7 +331,7 @@ class HTMLConformanceChecker(_base.Filter):
         # check for recognized tag name
         name = token.get("name", "").lower()
         if name not in allowedAttributeMap.keys():
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "unknown-start-tag",
                    "datavars": {"tagName": name}}
 
@@ -342,7 +343,7 @@ class HTMLConformanceChecker(_base.Filter):
                             in token.get("data", [])]
             for attrName in requiredAttributeMap[name]:
                 if attrName not in attrsPresent:
-                    yield {"type": "ParseError",
+                    yield {"type": tokenTypes["ParseError"],
                            "data": "missing-required-attribute",
                            "datavars": {"tagName": name,
                                         "attributeName": attrName}}
@@ -353,7 +354,7 @@ class HTMLConformanceChecker(_base.Filter):
         allowedAttributes = globalAttributes | allowedAttributeMap.get(name, frozenset(()))
         for attrName, attrValue in token.get("data", []):
             if attrName.lower() not in allowedAttributes:
-                yield {"type": "ParseError",
+                yield {"type": tokenTypes["ParseError"],
                        "data": "unknown-attribute",
                        "datavars": {"tagName": name,
                                     "attributeName": attrName}}
@@ -365,11 +366,11 @@ class HTMLConformanceChecker(_base.Filter):
 #    def checkURI(self, token, tagName, attrName, attrValue):
 #        isValid, errorCode = rfc3987.isValidURI(attrValue)
 #        if not isValid:
-#            yield {"type": "ParseError",
+#            yield {"type": tokenTypes["ParseError"],
 #                   "data": errorCode,
 #                   "datavars": {"tagName": tagName,
 #                                "attributeName": attrName}}
-#            yield {"type": "ParseError",
+#            yield {"type": tokenTypes["ParseError"],
 #                   "data": "invalid-attribute-value",
 #                   "datavars": {"tagName": tagName,
 #                                "attributeName": attrName}}
@@ -377,28 +378,28 @@ class HTMLConformanceChecker(_base.Filter):
     def checkIRI(self, token, tagName, attrName, attrValue):
         isValid, errorCode = rfc3987.isValidIRI(attrValue)
         if not isValid:
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": errorCode,
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName}}
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "invalid-attribute-value",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName}}
 
     def checkID(self, token, tagName, attrName, attrValue):
         if not attrValue:
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "attribute-value-can-not-be-blank",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName}}
         for c in attrValue:
             if c in spaceCharacters:
-                yield {"type": "ParseError",
+                yield {"type": tokenTypes["ParseError"],
                        "data": "space-in-id",
                        "datavars": {"tagName": tagName,
                                     "attributeName": attrName}}
-                yield {"type": "ParseError",
+                yield {"type": tokenTypes["ParseError"],
                        "data": "invalid-attribute-value",
                        "datavars": {"tagName": tagName,
                                     "attributeName": attrName}}
@@ -427,7 +428,7 @@ class HTMLConformanceChecker(_base.Filter):
         valueDict = {}
         for currentValue in valueList:
             if valueDict.has_key(currentValue):
-                yield {"type": "ParseError",
+                yield {"type": tokenTypes["ParseError"],
                        "data": "duplicate-value-in-token-list",
                        "datavars": {"tagName": tagName,
                                     "attributeName": attrName,
@@ -437,19 +438,19 @@ class HTMLConformanceChecker(_base.Filter):
 
     def checkEnumeratedValue(self, token, tagName, attrName, attrValue, enumeratedValues):
         if not attrValue and ('' not in enumeratedValues):
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "attribute-value-can-not-be-blank",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName}}
             return
         attrValue = attrValue.lower()
         if attrValue not in enumeratedValues:
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "invalid-enumerated-value",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName,
                                 "enumeratedValues": tuple(enumeratedValues)}}
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "invalid-attribute-value",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName}}
@@ -457,12 +458,12 @@ class HTMLConformanceChecker(_base.Filter):
     def checkBoolean(self, token, tagName, attrName, attrValue):
         enumeratedValues = frozenset((attrName, ''))
         if attrValue not in enumeratedValues:
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "invalid-boolean-value",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName,
                                 "enumeratedValues": tuple(enumeratedValues)}}
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "invalid-attribute-value",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName}}
@@ -471,7 +472,7 @@ class HTMLConformanceChecker(_base.Filter):
         sign = 1
         numberString = ''
         state = 'begin' # ('begin', 'initial-number', 'number', 'trailing-junk')
-        error = {"type": "ParseError",
+        error = {"type": tokenTypes["ParseError"],
                  "data": "invalid-integer-value",
                  "datavars": {"tagName": tagName,
                               "attributeName": attrName,
@@ -503,7 +504,7 @@ class HTMLConformanceChecker(_base.Filter):
             elif state == 'trailing-junk':
                 pass
         if not numberString:
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "attribute-value-can-not-be-blank",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName}}
@@ -517,7 +518,7 @@ class HTMLConformanceChecker(_base.Filter):
         if attrValue[0] != '_': return
         attrValue = attrValue.lower()
         if attrValue in frozenset(('_self', '_parent', '_top', '_blank')): return
-        yield {"type": "ParseError",
+        yield {"type": tokenTypes["ParseError"],
                "data": "invalid-browsing-context",
                "datavars": {"tagName": tagName,
                             "attributeName": attrName}}
@@ -525,7 +526,7 @@ class HTMLConformanceChecker(_base.Filter):
     def checkLangCode(self, token, tagName, attrName, attrValue):
         if not attrValue: return # blank is OK
         if not iso639codes.isValidLangCode(attrValue):
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "invalid-lang-code",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName,
@@ -534,13 +535,13 @@ class HTMLConformanceChecker(_base.Filter):
     def checkMIMEType(self, token, tagName, attrName, attrValue):
         # XXX needs tests
         if not attrValue:
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "attribute-value-can-not-be-blank",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName}}
 
         if not rfc2046.isValidMIMEType(attrValue):
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "invalid-mime-type",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName,
@@ -556,7 +557,7 @@ class HTMLConformanceChecker(_base.Filter):
         allowedValues = (tagName == 'link') and linkRelValues or aRelValues
         for currentValue in valueList:
             if currentValue not in allowedValues:
-                yield {"type": "ParseError",
+                yield {"type": tokenTypes["ParseError"],
                        "data": "invalid-rel",
                        "datavars": {"tagName": tagName,
                                     "attributeName": attrName}}
@@ -593,7 +594,7 @@ class HTMLConformanceChecker(_base.Filter):
     def validateAttributeValueClass(self, token, tagName, attrName, attrValue):
         for t in self.checkTokenList(tagName, attrName, attrValue) or []:
             yield t
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "invalid-attribute-value",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName}}
@@ -623,7 +624,7 @@ class HTMLConformanceChecker(_base.Filter):
         for t in self.checkID(token, tagName, attrName, attrValue) or []: yield t
         if not attrValue: return
         if attrValue in self.IDsWeHaveKnownAndLoved:
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "duplicate-id",
                    "datavars": {"tagName": tagName}}
         self.IDsWeHaveKnownAndLoved.append(attrValue)
@@ -641,7 +642,7 @@ class HTMLConformanceChecker(_base.Filter):
 
     def validateAttributeValueHtmlXmlns(self, token, tagName, attrName, attrValue):
         if attrValue != "http://www.w3.org/1999/xhtml":
-            yield {"type": "ParseError",
+            yield {"type": tokenTypes["ParseError"],
                    "data": "invalid-root-namespace",
                    "datavars": {"tagName": tagName,
                                 "attributeName": attrName}}
@@ -699,7 +700,7 @@ class HTMLConformanceChecker(_base.Filter):
                                       # hooray for obscure side effects!
             attrValue = attrsDict.get("contextmenu", "")
             if attrValue and (attrValue not in self.IDsWeHaveKnownAndLoved):
-                yield {"type": "ParseError",
+                yield {"type": tokenTypes["ParseError"],
                        "data": "id-does-not-exist",
                        "datavars": {"tagName": tagName,
                                     "attributeName": "contextmenu",
@@ -710,6 +711,6 @@ class HTMLConformanceChecker(_base.Filter):
                     if not id: continue
                     if id == attrValue:
                         if refToken.get("name", "").lower() != "menu":
-                            yield {"type": "ParseError",
+                            yield {"type": tokenTypes["ParseError"],
                                    "data": "contextmenu-must-point-to-menu"}
                         break

commit 10bea2ca97a31a3d69635ce91436ffbb2a1cfef7

Fixed a sanitiser regression from r1248
--HG--
extra : convert_revision : svn%3Aacbfec75-9323-0410-a652-858a13e371e0/trunk%401272

python/src/html5lib/sanitizer.py
@@ -152,7 +152,7 @@ class HTMLSanitizerMixin(object):
                             continue
                         val_unescaped = re.sub("[`\000-\040\177-\240\s]+", '',
                                                unescape(attrs[attr])).lower()
-                        if (re.match("^[a-z0-9][-+.a-z0-9]*:",val_unescaped) or
+                        if (re.match("^[a-z0-9][-+.a-z0-9]*:",val_unescaped) and
                             (val_unescaped.split(':')[0] not in 
                              self.allowed_protocols)):
                             del attrs[attr]